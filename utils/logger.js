b764dd02c90b6d1a24c38c78f16c391e20cbeb35
TIndris <indris@mhs.usk.ac.id>
2025-06-23 22:29:50 +0700

    add utils logger

--- utils/logger.js
+++ utils/logger.js
@@ -1,16 +1,8 @@
-import fs from 'fs';
-import path from 'path';
 import winston from 'winston';
-
-const logDir = path.resolve('logs');
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
-}
 
 const logger = winston.createLogger({
   transports: [
-    new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
-    new winston.transports.File({ filename: path.join(logDir, 'combined.log') })
+    new winston.transports.Console({ level: 'info' })
   ]
 });
 
